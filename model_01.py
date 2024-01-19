83026c153700d906952c6f60da7ae8fcc3418f8c
martin-lndbl-GTL <lblsolutions@outlook.de>
2024-01-19 12:49:19 +0900

    correct model order

--- model_01.py
+++ model_01.py
@@ -1,21 +1,19 @@
 import tensorflow as tf
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path='mnist.npz')
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = tf.keras.utils.normalize(x_train, axis = 1)
 x_test = tf.keras.utils.normalize(x_test, axis = 1)
 
-model = tf.keras.models.Sequential()
-
-# Convert 2D Matrix into Vector
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-
-# relu = rectify linear unit (range: [0, inf))
-model.add(tf.keras.layers.Dense(128, activation=tf.keras.activations.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.keras.activations.relu))
-
-# Map to actual output
-model.add(tf.keras.layers.Dense(10, activation=tf.keras.activations.softmax))
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
 
 model.compile(
     optimizer='adam',
@@ -24,4 +22,4 @@ model.compile(
 
 model.fit(x_train, y_train, epochs=3)
 
-model.save('model_01.keras')
+model.save('model_04.keras')
